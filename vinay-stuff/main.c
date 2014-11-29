bd28f76eb0646c14bfc6751dd248005ae313256f
vff6 <vff6@cornell.edu>
2014-11-29 15:09:48 -0500

    vinay - question marks in latency

--- vinay-stuff/main.c
+++ vinay-stuff/main.c
@@ -1,3 +1,5 @@
+#ifndef _black_scholes3_loop_H
+#define _black_scholes3_loop_H
 #include "black_scholes2.h"
 #include "gaussian.h"
 #include "parser.h"
@@ -128,6 +130,6 @@ main (int argc, char* argv[])
 
   return 0;
 }
-
+#endif
 
 
--- vinay-stuff/run2.tcl
+++ vinay-stuff/run2.tcl
@@ -7,6 +7,8 @@ add_files black_scholes2.c
 add_files gaussian.c
 add_files mt19937ar.c
 
+set_directive_loop_tripcount -min 0 -max 10000 -avg 500 black_scholes2_loop/pipe_loop
+
 open_solution "solution1"
 set_part {xc7z020clg484-1}
 create_clock -period 10
